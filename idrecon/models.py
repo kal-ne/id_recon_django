777aab476a66243b6622efe3dad0408291cbfaf5
Neeraj Krishnan K <9nkrish@gmail.com>
2024-07-28 03:29:07 +0530

    Add Contacts model

--- idrecon/models.py
+++ idrecon/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    class LinkPrecedence(models.TextChoices):
+        PRIMARY = "primary", "Primary"
+        SECONDARY = "secondary", "Secondary" # For localisation use _("Secondary")
+
+    id = models.IntegerField(primary_key=True)
+    phone_number = models.CharField(blank=True, null=True)
+    email = models.CharField(blank=True, null=True)
+    linked_id = models.IntegerField(blank=True, null=True)
+    link_precedence = models.CharField(max_length=1, choices=LinkPrecedence)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
