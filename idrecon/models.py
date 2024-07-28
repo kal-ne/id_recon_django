aa7a1a825cd35df3bb2525ad64dd08c8d7408526
Neeraj Krishnan K <9nkrish@gmail.com>
2024-07-28 19:03:35 +0530

    Update Contact model and makemigrations

--- idrecon/models.py
+++ idrecon/models.py
@@ -6,11 +6,13 @@ class Contact(models.Model):
         SECONDARY = "secondary", "Secondary" # For localisation use _("Secondary")
 
     id = models.IntegerField(primary_key=True)
-    phone_number = models.CharField(blank=True, null=True)
-    email = models.CharField(blank=True, null=True)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
     linked_id = models.IntegerField(blank=True, null=True)
-    link_precedence = models.CharField(max_length=1, choices=LinkPrecedence)
+    link_precedence = models.CharField(max_length=9, choices=LinkPrecedence)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
+    class Meta:
+        db_table = "Contact"
