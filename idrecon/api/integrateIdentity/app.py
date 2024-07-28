62496e99207fd4fb934c09d0febd4c5a4b6cc2e9
Neeraj Krishnan K <9nkrish@gmail.com>
2024-07-29 04:24:29 +0530

    Add code to app.py

--- idrecon/api/integrateIdentity/app.py
+++ idrecon/api/integrateIdentity/app.py
@@ -1,13 +1,159 @@
 import json
 from idrecon.models import Contact
+from django.core.exceptions import ObjectDoesNotExist
+
+# def CreateContact(**kwargs):
+#     email = kwargs.get('email', None)
+#     phone_number = kwargs.get('phone_number', None)
+#     return None
 
 def handler(event):
     body = json.loads(event.body)
+    req_email = body.get("email")
+    req_phone = body.get("phoneNumber")
+
+    if req_email is None and req_phone is None:
+        resp = {"Error": "Invalid Request"}
+        return resp
+    
+    elif req_email is None:
+        matched_phone = Contact.objects.filter(phone_number=req_phone).first() #, link_precedence = Contact.LinkPrecedence.PRIMARY)
+        if matched_phone is None:
+            new_contact = Contact(phone_number=req_phone, link_precedence=Contact.LinkPrecedence.PRIMARY)
+            saved_contact = new_contact.save()
+            matched_phone_pid = saved_contact.id
+        else:
+            if matched_phone.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                matched_phone_pid = matched_phone.id
+            else:
+                matched_phone_pid = matched_phone.linked_id
+        primary_id = matched_phone_pid
+
+    elif req_phone is None:
+        matched_email = Contact.objects.filter(email=req_email).first()
+        if matched_email is None:
+            new_contact = Contact(email=req_email, link_precedence=Contact.LinkPrecedence.PRIMARY)
+            new_contact.save()
+            matched_email_pid = new_contact.id
+        else:
+            if matched_email.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                matched_email_pid = matched_email.id
+            else:
+                matched_email_pid = matched_email.linked_id
+        primary_id = matched_email_pid
+
+    else:
+        matched_phone = Contact.objects.filter(phone_number=req_phone).first()
+        matched_email = Contact.objects.filter(email=req_email).first()
+
+        if matched_phone is None and matched_email is None:
+            new_contact = Contact(phone_number=req_phone,
+                                    email=req_email,
+                                    link_precedence=Contact.LinkPrecedence.PRIMARY
+                                )
+            new_contact.save()
+            matched_pid = new_contact.id
+        elif matched_email is None:
+            if matched_phone.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                matched_phone_pid = matched_phone.id
+            else:
+                matched_phone_pid = matched_phone.linked_id
+            new_contact = Contact(phone_number=req_phone,
+                                    email=req_email,
+                                    linked_id=matched_phone_pid,
+                                    link_precedence=Contact.LinkPrecedence.SECONDARY
+                                )
+            new_contact.save()
+            matched_pid = new_contact.id
+        elif matched_phone is None:
+            if matched_email.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                matched_email_pid = matched_email.id
+            else:
+                matched_email_pid = matched_email.linked_id
+            new_contact = Contact(phone_number=req_phone,
+                                    email=req_email,
+                                    linked_id=matched_email_pid,
+                                    link_precedence=Contact.LinkPrecedence.SECONDARY
+                                )
+            new_contact.save()
+            matched_pid = new_contact.id
+        else:
+            if matched_phone.id == matched_email.id:
+                if matched_email.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                    matched_pid = matched_email.id
+                else:
+                    matched_pid = matched_email.linked_id
+            else:
+                # Get id of Primary of contact with Phone Number match
+                if matched_phone.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                    matched_phone_pid = matched_phone.id
+                else:
+                    matched_phone_pid = matched_phone.linked_id
+
+                # Get id of Primary of contact with email match
+                if matched_email.link_precedence == Contact.LinkPrecedence.PRIMARY:
+                    matched_email_pid = matched_email.id
+                else:
+                    matched_email_pid = matched_email.linked_id
+                
+                if matched_phone_pid > matched_email_pid:
+                    # Get old Primary of contact with Phone Number match
+                    matched_phone_op = Contact.objects.get(id=matched_phone_pid)
+                    
+                    # Set op to Secondary and link to new primary
+                    matched_phone_op.link_precedence = Contact.LinkPrecedence.SECONDARY
+                    matched_phone_op.linked_id = matched_email_pid
+                    matched_phone_op.save()
+
+                    matched_pid = matched_email_pid
+                    # Link every secondary of op to new primary
+                    Contact.objects.filter(linked_id=matched_phone_pid).update(linked_id=matched_pid)
+                else:
+                    # Get old Primary of contact with email match
+                    matched_email_op = Contact.objects.get(id=matched_email_pid)
+
+                    # Set op to Secondary and link to new primary
+                    matched_email_op.link_precedence = Contact.LinkPrecedence.SECONDARY
+                    matched_email_op.linked_id = matched_phone_pid
+                    matched_email_op.save()
+                    
+                    matched_pid = matched_phone_pid
+                    # Link every secondary of op to new primary
+                    Contact.objects.filter(linked_id=matched_email_pid).update(linked_id=matched_pid)
+        primary_id = matched_pid
+        
+ 
+
+    # Initialize the lists as empty
+    email_array = []
+    phone_array = []
+    secondary_contact_ids = []
+
+    # Get primary email and phone number
+    try:
+        primary_contact = Contact.objects.get(id=primary_id)
+        email_array.append(primary_contact.email)
+        phone_array.append(primary_contact.phone_number)
+    except ObjectDoesNotExist:
+        pass
+
+    # Get all emails of this contact to include in response
+    other_emails = Contact.objects.filter(linked_id=primary_id).values_list('email', flat=True)
+    email_array.extend(other_emails)
+
+    # Get all phone numbers of this contact to include in response
+    other_phones = Contact.objects.filter(linked_id=primary_id).values_list('phone_number', flat=True)
+    phone_array.extend(other_phones)
+
+    other_ids = Contact.objects.filter(linked_id=primary_id).values_list('id', flat=True)
+    secondary_contact_ids.extend(other_ids)
+
+    # Prepare response and return it
     contactResp = {
-        "primaryContactID": 0,
-        "emails": [].append(body["email"]),
-        "phoneNumbers": [].append(body["phoneNumber"]),
-        "secondaryContactIds": []
+        "primaryContactID": primary_id,
+        "emails": email_array,
+        "phoneNumbers": phone_array,
+        "secondaryContactIds": secondary_contact_ids
     }
     resp = {"contact": contactResp}
     return resp
